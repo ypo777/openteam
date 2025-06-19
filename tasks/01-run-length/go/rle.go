81492ef649cd740dfa1b7f0f0f837e1290299778
ypo <yanpaingoo008ypo@gmail.com>
2025-06-19 09:18:04 +0700

    fix -> bug 01
    - golang
    - time - 12 mins
    - ai : cursor

--- tasks/01-run-length/go/rle.go
+++ tasks/01-run-length/go/rle.go
@@ -1,9 +1,36 @@
 package rle
 
+import (
+	"strconv"
+	"strings"
+)
 // Encode returns the run‑length encoding of UTF‑8 string s.
 //
 // "AAB" → "A2B1"
 func Encode(s string) string {
-	// TODO: implement
-	panic("implement me")
+	if len(s) == 0 {
+		return ""
+	}
+
+	var result strings.Builder
+	runes := []rune(s)
+
+	currentChar := runes[0]
+	count := 1
+
+	for i:=1; i < len(runes); i++{
+		if runes[i] == currentChar {
+			count++
+		}else{
+			result.WriteRune(currentChar)
+			result.WriteString(strconv.Itoa(count))
+
+			currentChar = runes[i]
+			count = 1
+		}
+	}
+	result.WriteRune(currentChar)
+	result.WriteString(strconv.Itoa(count))
+
+	return result.String()
 }
